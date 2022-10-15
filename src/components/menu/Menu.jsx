9cb2a4ac5935b852f36daa430225f47b64de6813
didina44 <didina44@gmail.com>
2022-10-15 04:57:47 +0300

    Add files via upload

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -12,15 +12,13 @@ export default function Menu({ menuOpen, setMenuOpen }) {
           <a><HashLink to= "/#intro" className="menuLinks" style={{textTransform: "capitalize"}} >Acasa</HashLink></a>
         </li>
         <li onClick={()=>setMenuOpen(false)}>
-<a> <HashLink to= "/#portfolio" className="menuLinks" style={{textTransform: "capitalize"}} >Prezentare </HashLink></a>
+<a> <HashLink to= "/#portfolio" className="menuLinks" style={{textTransform: "capitalize"}} >Prezentare si ingrediente</HashLink></a>
         </li>
+        
         <li onClick={()=>setMenuOpen(false)}>
-<a><Link to='/produse' className="menuLinks" style={{textTransform: "capitalize"}} >Produse </Link></a>
-        </li>
+<a> <HashLink to= "/#works" className="menuLinks">Preparare</HashLink></a>        </li>
         <li onClick={()=>setMenuOpen(false)}>
-<a> <HashLink to= "/#works" className="menuLinks">Servicii</HashLink></a>        </li>
-        <li onClick={()=>setMenuOpen(false)}>
-<a ><HashLink to= "/#testimonials" className="menuLinks">Despre noi</HashLink></a>
+<a ><HashLink to= "/#testimonials" className="menuLinks">Despre mine</HashLink></a>
         </li>
          <li onClick={()=>setMenuOpen(false)}>
 <a><HashLink to= "/#contact" className="menuLinks">Contact</HashLink></a>
